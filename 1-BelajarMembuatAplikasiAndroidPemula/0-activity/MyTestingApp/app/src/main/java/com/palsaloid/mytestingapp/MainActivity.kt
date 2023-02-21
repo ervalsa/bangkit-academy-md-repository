9bf22a308b3878a4b9dfceb39434f61cfa290e3b
ervalsa-san <ervalsananda@gmail.com>
2023-02-22 03:41:28 +0800

    Finish Debugging dengan Logging dan Debug Breakpoint

--- 1-BelajarMembuatAplikasiAndroidPemula/0-activity/MyTestingApp/app/src/main/java/com/palsaloid/mytestingapp/MainActivity.kt
+++ 1-BelajarMembuatAplikasiAndroidPemula/0-activity/MyTestingApp/app/src/main/java/com/palsaloid/mytestingapp/MainActivity.kt
@@ -2,10 +2,39 @@ package com.palsaloid.mytestingapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var btnSetValue: Button
+    private lateinit var tvText: TextView
+
+    private var names = ArrayList<String>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnSetValue = findViewById(R.id.btn_set_value)
+        tvText = findViewById(R.id.tv_text)
+        btnSetValue.setOnClickListener(this)
+
+        names.add("Narenda Wicaksono")
+        names.add("Kevin")
+        names.add("Yoza")
+    }
+
+    override fun onClick(v: View) {
+        if (v.id == R.id.btn_set_value) {
+            Log.d("MainActivity", names.toString())
+            val name = StringBuilder()
+            for (i in 0..2) {
+                name.append(names[i]).append("\n")
+            }
+            tvText.text = name.toString()
+        }
     }
 }
