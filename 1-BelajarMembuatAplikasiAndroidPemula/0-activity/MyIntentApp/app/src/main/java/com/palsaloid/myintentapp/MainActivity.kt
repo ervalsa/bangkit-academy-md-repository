c9c3f7583954c0da946cb8dac0cbd0b87d5d8ea1
ervalsa-san <ervalsananda@gmail.com>
2023-02-22 02:19:41 +0800

    Finish Latihan Explicit Intent

--- 1-BelajarMembuatAplikasiAndroidPemula/0-activity/MyIntentApp/app/src/main/java/com/palsaloid/myintentapp/MainActivity.kt
+++ 1-BelajarMembuatAplikasiAndroidPemula/0-activity/MyIntentApp/app/src/main/java/com/palsaloid/myintentapp/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.palsaloid.myintentapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnMoveActivity: Button = findViewById(R.id.btn_move_activity)
+        btnMoveActivity.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.btn_move_activity -> {
+                val moveIntent = Intent(this@MainActivity, MoveActivity::class.java)
+                startActivity(moveIntent)
+            }
+        }
     }
 }
