370ab4d1ed1a8f81aec8e7f20e182aa51affe0ce
ervalsa-san <ervalsananda@gmail.com>
2023-02-22 02:47:02 +0800

    Finish Latihan Mengirim Data dengan Parcelable

--- 1-BelajarMembuatAplikasiAndroidPemula/0-activity/MyIntentApp/app/src/main/java/com/palsaloid/myintentapp/MainActivity.kt
+++ 1-BelajarMembuatAplikasiAndroidPemula/0-activity/MyIntentApp/app/src/main/java/com/palsaloid/myintentapp/MainActivity.kt
@@ -16,6 +16,9 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
         val btnMoveWithDataActivity: Button = findViewById(R.id.btn_move_activity_data)
         btnMoveWithDataActivity.setOnClickListener(this )
+
+        val btnMoveWithObject: Button = findViewById(R.id.btn_move_activity_object)
+        btnMoveWithObject.setOnClickListener(this)
     }
 
     override fun onClick(v: View?) {
@@ -30,6 +33,19 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                 moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_NAME, "DicodingAcademy Boy")
                 moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_AGE, 5)
                 startActivity(moveWithDataIntent)
+            }
+
+            R.id.btn_move_activity_object -> {
+                val person = Person(
+                    "DicodingAcademy",
+                    5,
+                    "academy@dicoding.com",
+                    "Bandung"
+                )
+
+                val moveWithDataObjectIntent = Intent(this@MainActivity, MoveWithObjectActivity::class.java)
+                moveWithDataObjectIntent.putExtra(MoveWithObjectActivity.EXTRA_PERSON, person)
+                startActivity(moveWithDataObjectIntent)
             }
         }
     }
---
+++ 1-BelajarMembuatAplikasiAndroidPemula/0-activity/MyIntentApp/app/src/main/java/com/palsaloid/myintentapp/MoveWithObjectActivity.kt
@@ -0,0 +1,32 @@
+package com.palsaloid.myintentapp
+
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MoveWithObjectActivity : AppCompatActivity() {
+
+    companion object {
+        const val EXTRA_PERSON = "extra_person"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_move_with_object)
+
+        val tvObject: TextView = findViewById(R.id.tv_object_recevied)
+
+        val person = if (Build.VERSION.SDK_INT >= 33) {
+            intent.getParcelableExtra(EXTRA_PERSON, Person::class.java)
+        } else {
+            @Suppress("DEPRECATION")
+            intent.getParcelableExtra(EXTRA_PERSON)
+        }
+
+        if (person != null) {
+            val text = "Name : ${person.name.toString()},\nEmail : ${person.email},\nAge : ${person.age},\nLocation : ${person.city}"
+            tvObject.text = text
+        }
+    }
+}
