d5e167e907f3f3db319a537915da89d132ca8bce
ervalsa-san <ervalsananda@gmail.com>
2023-02-23 01:14:03 +0800

    Finish create recyclerview

---
+++ 1-BelajarMembuatAplikasiAndroidPemula/4-recyclerview/MyRecyclerView/app/src/main/java/com/palsaloid/myrecyclerview/Hero.kt
@@ -0,0 +1,11 @@
+package com.palsaloid.myrecyclerview
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class Hero(
+    var name: String,
+    var description: String,
+    var photo: Int
+) : Parcelable
--- 1-BelajarMembuatAplikasiAndroidPemula/4-recyclerview/MyRecyclerView/app/src/main/java/com/palsaloid/myrecyclerview/MainActivity.kt
+++ 1-BelajarMembuatAplikasiAndroidPemula/4-recyclerview/MyRecyclerView/app/src/main/java/com/palsaloid/myrecyclerview/MainActivity.kt
@@ -2,10 +2,42 @@ package com.palsaloid.myrecyclerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var rvHeroes: RecyclerView
+    private val list = ArrayList<Hero>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        rvHeroes = findViewById(R.id.rv_heroes)
+        rvHeroes.setHasFixedSize(true)
+
+        list.addAll(getListHeroes())
+        showRecyclerList()
+    }
+
+    private fun showRecyclerList() {
+        rvHeroes.layoutManager = LinearLayoutManager(this)
+        val listHeroeAdapter = ListHeroAdapter(list)
+        rvHeroes.adapter = listHeroeAdapter
+    }
+
+    private fun getListHeroes(): ArrayList<Hero> {
+        val dataName = resources.getStringArray(R.array.data_name)
+        val dataDescription = resources.getStringArray(R.array.data_description)
+        val dataPhoto = resources.obtainTypedArray(R.array.data_photo)
+
+        val listHero = ArrayList<Hero>()
+        for (i in dataName.indices) {
+            val hero = Hero(dataName[i], dataDescription[i], dataPhoto.getResourceId(i, -1))
+            listHero.add(hero)
+        }
+
+        return listHero
     }
 }
