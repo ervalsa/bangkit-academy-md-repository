8ccae8c7f4a462ebf2cd2bdf4d66e0857e83de0a
ervalsa-san <ervalsananda@gmail.com>
2023-02-23 01:46:41 +0800

    Finish Latihan RecyclerView

--- 1-BelajarMembuatAplikasiAndroidPemula/4-recyclerview/MyRecyclerView/app/src/main/java/com/palsaloid/myrecyclerview/MainActivity.kt
+++ 1-BelajarMembuatAplikasiAndroidPemula/4-recyclerview/MyRecyclerView/app/src/main/java/com/palsaloid/myrecyclerview/MainActivity.kt
@@ -2,7 +2,10 @@ package com.palsaloid.myrecyclerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Toast
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -20,6 +23,24 @@ class MainActivity : AppCompatActivity() {
 
         list.addAll(getListHeroes())
         showRecyclerList()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_list -> {
+                rvHeroes.layoutManager = LinearLayoutManager(this)
+            }
+
+            R.id.action_grid -> {
+                rvHeroes.layoutManager = GridLayoutManager(this, 2)
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     private fun showRecyclerList() {
