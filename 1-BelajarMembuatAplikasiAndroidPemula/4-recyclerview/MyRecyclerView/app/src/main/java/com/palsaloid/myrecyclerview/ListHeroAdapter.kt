7fe649f153702f89913354dc5ea948a1ce6c366c
ervalsa-san <ervalsananda@gmail.com>
2023-02-23 08:26:49 +0800

    Add Glide and Implement to Project

--- 1-BelajarMembuatAplikasiAndroidPemula/4-recyclerview/MyRecyclerView/app/src/main/java/com/palsaloid/myrecyclerview/ListHeroAdapter.kt
+++ 1-BelajarMembuatAplikasiAndroidPemula/4-recyclerview/MyRecyclerView/app/src/main/java/com/palsaloid/myrecyclerview/ListHeroAdapter.kt
@@ -7,6 +7,7 @@ import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 
 class ListHeroAdapter(private val listHero: ArrayList<Hero>) : RecyclerView.Adapter<ListHeroAdapter.ListViewHolder>() {
 
@@ -23,7 +24,9 @@ class ListHeroAdapter(private val listHero: ArrayList<Hero>) : RecyclerView.Adap
 
     override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
         val (name, description, photo) = listHero[position]
-        holder.imgPhoto.setImageResource(photo)
+        Glide.with(holder.itemView.context)
+            .load(photo)
+            .into(holder.imgPhoto)
         holder.tvName.text = name
         holder.tvDescription.text = description
 
--- 1-BelajarMembuatAplikasiAndroidPemula/4-recyclerview/MyRecyclerView/app/src/main/java/com/palsaloid/myrecyclerview/MainActivity.kt
+++ 1-BelajarMembuatAplikasiAndroidPemula/4-recyclerview/MyRecyclerView/app/src/main/java/com/palsaloid/myrecyclerview/MainActivity.kt
@@ -58,11 +58,11 @@ class MainActivity : AppCompatActivity() {
     private fun getListHeroes(): ArrayList<Hero> {
         val dataName = resources.getStringArray(R.array.data_name)
         val dataDescription = resources.getStringArray(R.array.data_description)
-        val dataPhoto = resources.obtainTypedArray(R.array.data_photo)
+        val dataPhoto = resources.getStringArray(R.array.data_photo)
 
         val listHero = ArrayList<Hero>()
         for (i in dataName.indices) {
-            val hero = Hero(dataName[i], dataDescription[i], dataPhoto.getResourceId(i, -1))
+            val hero = Hero(dataName[i], dataDescription[i], dataPhoto[i])
             listHero.add(hero)
         }
 
