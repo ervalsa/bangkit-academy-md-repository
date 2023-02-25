cc9da6bc2945c3293ca0a338f81cac6656a2247f
ervalsa-san <ervalsananda@gmail.com>
2023-02-26 00:27:19 +0800

    Finish Tablayout project

---
+++ 2-BelajarFundamentalAplikasiAndroid/1-navigation/MyTabLayout/app/src/main/java/com/palsaloid/mytablayout/SectionsPagerAdapter.kt
@@ -0,0 +1,21 @@
+package com.palsaloid.mytablayout
+
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class SectionsPagerAdapter(activity: AppCompatActivity) : FragmentStateAdapter(activity) {
+
+    override fun createFragment(position: Int): Fragment {
+        var fragment: Fragment? = null
+        when (position) {
+            0 -> fragment = HomeFragment()
+            1 -> fragment = ProfileFragment()
+        }
+        return fragment as Fragment
+    }
+
+    override fun getItemCount(): Int {
+        return 2
+    }
+}
