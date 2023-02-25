bfe46a830c059bd8ac7dbdb0c97019eb4806996c
ervalsa-san <ervalsananda@gmail.com>
2023-02-25 23:24:56 +0800

    Finish searchview

--- 2-BelajarFundamentalAplikasiAndroid/1-navigation/MyActionBar/app/src/main/java/com/palsaloid/myactionbar/MainActivity.kt
+++ 2-BelajarFundamentalAplikasiAndroid/1-navigation/MyActionBar/app/src/main/java/com/palsaloid/myactionbar/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.palsaloid.myactionbar
 
+import android.app.SearchManager
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.widget.SearchView
 import com.palsaloid.myactionbar.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -21,6 +25,24 @@ class MainActivity : AppCompatActivity() {
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         val inflater = menuInflater
         inflater.inflate(R.menu.option_menu, menu)
+
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        val searchView = menu.findItem(R.id.search).actionView as SearchView
+
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(componentName))
+        searchView.queryHint = resources.getString(R.string.search_hint)
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String): Boolean {
+                Toast.makeText(this@MainActivity, query, Toast.LENGTH_SHORT).show()
+                searchView.clearFocus()
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return true
+            }
+        })
+
         return true
     }
 
