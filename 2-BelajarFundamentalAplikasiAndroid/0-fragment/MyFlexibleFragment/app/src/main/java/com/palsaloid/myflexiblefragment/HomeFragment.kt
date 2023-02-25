50b2e055590747d81dc13a6707f161ad670110e7
ervalsa-san <ervalsananda@gmail.com>
2023-02-25 14:55:48 +0800

    Finish latihan fragment di activity

---
+++ 2-BelajarFundamentalAplikasiAndroid/0-fragment/MyFlexibleFragment/app/src/main/java/com/palsaloid/myflexiblefragment/HomeFragment.kt
@@ -0,0 +1,29 @@
+package com.palsaloid.myflexiblefragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+
+class HomeFragment : Fragment(), View.OnClickListener {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val btnCategory: Button = view.findViewById(R.id.btn_category)
+        btnCategory.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        TODO("Not yet implemented")
+    }
+}
