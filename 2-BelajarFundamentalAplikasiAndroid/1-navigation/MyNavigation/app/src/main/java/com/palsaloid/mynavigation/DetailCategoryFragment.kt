bc103147521df6e558c598ce4e476807de9046ae
ervalsa-san <ervalsananda@gmail.com>
2023-02-25 22:50:25 +0800

    pop up behaviour

--- 2-BelajarFundamentalAplikasiAndroid/1-navigation/MyNavigation/app/src/main/java/com/palsaloid/mynavigation/DetailCategoryFragment.kt
+++ 2-BelajarFundamentalAplikasiAndroid/1-navigation/MyNavigation/app/src/main/java/com/palsaloid/mynavigation/DetailCategoryFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import com.palsaloid.mynavigation.databinding.FragmentDetailCategoryBinding
 
 class DetailCategoryFragment : Fragment() {
@@ -29,6 +30,10 @@ class DetailCategoryFragment : Fragment() {
 
         binding.tvCategoryName.text = dataName
         binding.tvCategoryDescription.text = "Stock : $dataDescription"
+
+        binding.btnProfile.setOnClickListener (
+            Navigation.createNavigateOnClickListener(R.id.action_detailCategoryFragment_to_homeFragment)
+                )
     }
 
     override fun onDestroy() {
