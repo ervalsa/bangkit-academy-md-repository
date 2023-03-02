dee0b466394dd00a1b59e3989124420017bc3659
ervalsa-san <ervalsananda@gmail.com>
2023-03-02 13:30:58 +0800

    Finish BackgroundThread Project

--- 2-BelajarFundamentalAplikasiAndroid/2-backgroundthreaddannetworking/MyBackgroundThread/app/src/main/java/com/palsaloid/mybackgroundthread/MainActivity.kt
+++ 2-BelajarFundamentalAplikasiAndroid/2-backgroundthreaddannetworking/MyBackgroundThread/app/src/main/java/com/palsaloid/mybackgroundthread/MainActivity.kt
@@ -2,10 +2,39 @@ package com.palsaloid.mybackgroundthread
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.Button
+import android.widget.TextView
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.util.concurrent.Executors
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnStart = findViewById<Button>(R.id.btn_start)
+        val tvStatus = findViewById<TextView>(R.id.tv_status)
+
+        btnStart.setOnClickListener {
+            lifecycleScope.launch(Dispatchers.Default) {
+                for (i in 0..10) {
+                    delay(500)
+                    val percentage = i * 10
+                    withContext(Dispatchers.Main) {
+                        if (percentage == 100) {
+                            tvStatus.setText(R.string.task_completed)
+                        } else {
+                            tvStatus.text = String.format(getString(R.string.compressing), percentage)
+                        }
+                    }
+                }
+            }
+        }
     }
 }
