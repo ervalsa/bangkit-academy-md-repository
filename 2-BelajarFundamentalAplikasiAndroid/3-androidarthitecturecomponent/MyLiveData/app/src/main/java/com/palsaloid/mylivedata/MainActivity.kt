4bdc42e6173de70aca202b877224f39d0fc39ea9
ervalsa-san <ervalsananda@gmail.com>
2023-03-03 08:46:27 +0800

    Finish LiveData Project

--- 2-BelajarFundamentalAplikasiAndroid/3-androidarthitecturecomponent/MyLiveData/app/src/main/java/com/palsaloid/mylivedata/MainActivity.kt
+++ 2-BelajarFundamentalAplikasiAndroid/3-androidarthitecturecomponent/MyLiveData/app/src/main/java/com/palsaloid/mylivedata/MainActivity.kt
@@ -2,10 +2,30 @@ package com.palsaloid.mylivedata
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.palsaloid.mylivedata.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mLiveDataTimerViewModel: MainViewModel
+    private lateinit var activityMainBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityMainBinding.root)
+
+        mLiveDataTimerViewModel = ViewModelProvider(this)[MainViewModel::class.java]
+        subscribe()
+    }
+
+    private fun subscribe() {
+        val elapsedTimeObserver = Observer<Long?> { aLong ->
+            val newText = this.resources.getString(R.string.seconds, aLong)
+            activityMainBinding.timerTextview.text = newText
+        }
+
+        mLiveDataTimerViewModel.getElapsedTime().observe(this, elapsedTimeObserver)
     }
 }
---
+++ 2-BelajarFundamentalAplikasiAndroid/3-androidarthitecturecomponent/MyLiveData/app/src/main/java/com/palsaloid/mylivedata/MainViewModel.kt
@@ -0,0 +1,32 @@
+package com.palsaloid.mylivedata
+
+import android.os.SystemClock
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import java.util.Timer
+import java.util.TimerTask
+
+class MainViewModel : ViewModel() {
+
+    companion object {
+        private const val ONE_SECOND = 1000
+    }
+
+    private val mInitialTime = SystemClock.elapsedRealtime()
+    private val mElapsedTime = MutableLiveData<Long?>()
+
+    init {
+        val timer = Timer()
+        timer.scheduleAtFixedRate(object : TimerTask() {
+            override fun run() {
+                val newValue = (SystemClock.elapsedRealtime() - mInitialTime) / 1000
+                mElapsedTime.postValue(newValue)
+            }
+        }, ONE_SECOND.toLong(), ONE_SECOND.toLong())
+    }
+
+    fun getElapsedTime(): LiveData<Long?> {
+        return mElapsedTime
+    }
+}
