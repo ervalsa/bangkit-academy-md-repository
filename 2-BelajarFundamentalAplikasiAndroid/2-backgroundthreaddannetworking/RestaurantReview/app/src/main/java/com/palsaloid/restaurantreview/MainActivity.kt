f03a75b1608ab2ba28a6396343906105ab3c4f48
ervalsa-san <ervalsananda@gmail.com>
2023-03-05 15:24:27 +0800

    viewmodel using ktx

--- 2-BelajarFundamentalAplikasiAndroid/2-backgroundthreaddannetworking/RestaurantReview/app/src/main/java/com/palsaloid/restaurantreview/MainActivity.kt
+++ 2-BelajarFundamentalAplikasiAndroid/2-backgroundthreaddannetworking/RestaurantReview/app/src/main/java/com/palsaloid/restaurantreview/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.view.inputmethod.InputMethodManager
+import androidx.activity.viewModels
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -26,6 +27,7 @@ import retrofit2.Response
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private val mainViewModel by viewModels<MainViewModel>()
 
     companion object {
         private const val TAG = "MainActivity"
@@ -39,8 +41,6 @@ class MainActivity : AppCompatActivity() {
 
         supportActionBar?.hide()
 
-        val mainViewModel = ViewModelProvider(this, ViewModelProvider.NewInstanceFactory())
-            .get(MainViewModel::class.java)
         mainViewModel.restaurant.observe(this, { restaurant ->
             setRestaurantData(restaurant)
         })
