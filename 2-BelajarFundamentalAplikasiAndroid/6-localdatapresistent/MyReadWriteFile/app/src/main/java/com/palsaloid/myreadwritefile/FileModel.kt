d0a412f23c0fcab3ca6372bade71dcaca25bca10
ervalsa-san <ervalsananda@gmail.com>
2023-03-16 10:55:14 +0800

    Finish ReadWrite App

---
+++ 2-BelajarFundamentalAplikasiAndroid/6-localdatapresistent/MyReadWriteFile/app/src/main/java/com/palsaloid/myreadwritefile/FileModel.kt
@@ -0,0 +1,6 @@
+package com.palsaloid.myreadwritefile
+
+class FileModel {
+    var filename: String? = null
+    var data: String? = null
+}
--- 2-BelajarFundamentalAplikasiAndroid/6-localdatapresistent/MyReadWriteFile/app/src/main/java/com/palsaloid/myreadwritefile/MainActivity.kt
+++ 2-BelajarFundamentalAplikasiAndroid/6-localdatapresistent/MyReadWriteFile/app/src/main/java/com/palsaloid/myreadwritefile/MainActivity.kt
@@ -2,10 +2,71 @@ package com.palsaloid.myreadwritefile
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import com.palsaloid.myreadwritefile.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonNew.setOnClickListener(this)
+        binding.buttonOpen.setOnClickListener(this)
+        binding.buttonSave.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        when(v.id) {
+            R.id.button_new -> newFile()
+            R.id.button_open -> showList()
+            R.id.button_save -> saveFile()
+        }
+    }
+
+    private fun saveFile() {
+        when {
+            binding.editTitle.text.toString().isEmpty() -> Toast.makeText(this, "Title harus diisi terlebih dahulu", Toast.LENGTH_SHORT).show()
+            binding.editFile.text.toString().isEmpty() -> Toast.makeText(this, "Konten harus diisi terlebih dahulu", Toast.LENGTH_SHORT).show()
+
+            else -> {
+                val title = binding.editTitle.text.toString()
+                val text = binding.editFile.text.toString()
+
+                val fileModel = FileModel()
+                fileModel.filename = title
+                fileModel.data = text
+
+                FileHelper.writeToFile(fileModel, this)
+                Toast.makeText(this, "Saving " + fileModel.filename + " file", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun loadData(title: String) {
+        val fileModel = FileHelper.readFromFile(this, title)
+        binding.editFile.setText(fileModel.filename)
+        binding.editFile.setText(fileModel.data)
+        Toast.makeText(this, "Loading " + fileModel.filename + " data", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun showList() {
+        val items = fileList()
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Pilih file yang diinginkan")
+        builder.setItems(items) { dialog, item -> loadData(items[item].toString()) }
+        val alert = builder.create()
+        alert.show()
+    }
+
+    private fun newFile() {
+        binding.editFile.setText("")
+        binding.editFile.setText("")
+        Toast.makeText(this, "Clearing file", Toast.LENGTH_SHORT).show()
     }
 }
