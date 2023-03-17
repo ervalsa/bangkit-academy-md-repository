ff8054187836e3cae83605145f81e1934edb3ecd
ervalsa-san <ervalsananda@gmail.com>
2023-03-17 15:38:08 +0800

    Finish SettingPreference

--- 2-BelajarFundamentalAplikasiAndroid/6-localdatapresistent/MySettingPreference/app/src/main/java/com/palsaloid/mysettingpreference/MainActivity.kt
+++ 2-BelajarFundamentalAplikasiAndroid/6-localdatapresistent/MySettingPreference/app/src/main/java/com/palsaloid/mysettingpreference/MainActivity.kt
@@ -7,5 +7,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportFragmentManager
+            .beginTransaction()
+            .add(R.id.setting_holder, MyPreferenceFragment())
+            .commit()
     }
 }
---
+++ 2-BelajarFundamentalAplikasiAndroid/6-localdatapresistent/MySettingPreference/app/src/main/java/com/palsaloid/mysettingpreference/MyPreferenceFragment.kt
@@ -0,0 +1,91 @@
+package com.palsaloid.mysettingpreference
+
+import android.content.SharedPreferences
+import android.os.Bundle
+import androidx.preference.CheckBoxPreference
+import androidx.preference.EditTextPreference
+import androidx.preference.PreferenceFragmentCompat
+
+class MyPreferenceFragment : PreferenceFragmentCompat(), SharedPreferences.OnSharedPreferenceChangeListener {
+
+    private lateinit var NAME: String
+    private lateinit var EMAIL: String
+    private lateinit var AGE: String
+    private lateinit var PHONE: String
+    private lateinit var LOVE: String
+
+    private lateinit var namePreference: EditTextPreference
+    private lateinit var emailPrefernce: EditTextPreference
+    private lateinit var agePreference: EditTextPreference
+    private lateinit var phonePreference: EditTextPreference
+    private lateinit var isLoveMuPreference: CheckBoxPreference
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        addPreferencesFromResource(R.xml.preferences)
+        init()
+        setSummaries()
+    }
+
+    private fun setSummaries() {
+        val sh = preferenceManager.sharedPreferences
+        namePreference.summary = sh?.getString(NAME, DEFAULT_VALUE)
+        emailPrefernce.summary = sh?.getString(EMAIL, DEFAULT_VALUE)
+        agePreference.summary = sh?.getString(AGE, DEFAULT_VALUE)
+        phonePreference.summary = sh?.getString(PHONE, DEFAULT_VALUE)
+        isLoveMuPreference.isChecked = sh?.getBoolean(LOVE, false) == true
+    }
+
+    private fun init() {
+        NAME = resources.getString(R.string.key_name)
+        EMAIL = resources.getString(R.string.key_email)
+        AGE = resources.getString(R.string.key_age)
+        PHONE = resources.getString(R.string.key_phone)
+        LOVE = resources.getString(R.string.key_love)
+
+        namePreference = findPreference<EditTextPreference>(NAME) as EditTextPreference
+        emailPrefernce = findPreference<EditTextPreference>(EMAIL) as EditTextPreference
+        agePreference = findPreference<EditTextPreference>(AGE) as EditTextPreference
+        phonePreference = findPreference<EditTextPreference>(PHONE) as EditTextPreference
+        isLoveMuPreference = findPreference<CheckBoxPreference>(LOVE) as CheckBoxPreference
+    }
+
+    override fun onResume() {
+        super.onResume()
+        preferenceScreen
+            .sharedPreferences
+            ?.registerOnSharedPreferenceChangeListener(this)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        preferenceScreen
+            .sharedPreferences
+            ?.unregisterOnSharedPreferenceChangeListener(this)
+    }
+
+    companion object {
+        private const val DEFAULT_VALUE = "Tidak Ada"
+    }
+
+    override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences?, key: String?) {
+        if (key == NAME) {
+            namePreference.summary = sharedPreferences?.getString(NAME, DEFAULT_VALUE)
+        }
+
+        if (key == EMAIL) {
+            emailPrefernce.summary = sharedPreferences?.getString(EMAIL, DEFAULT_VALUE)
+        }
+
+        if (key == AGE) {
+            agePreference.summary = sharedPreferences?.getString(AGE, DEFAULT_VALUE)
+        }
+
+        if (key == PHONE) {
+            phonePreference.summary = sharedPreferences?.getString(PHONE, DEFAULT_VALUE)
+        }
+
+        if (key == LOVE) {
+            isLoveMuPreference.isChecked = sharedPreferences?.getBoolean(LOVE, false) == true
+        }
+    }
+}
