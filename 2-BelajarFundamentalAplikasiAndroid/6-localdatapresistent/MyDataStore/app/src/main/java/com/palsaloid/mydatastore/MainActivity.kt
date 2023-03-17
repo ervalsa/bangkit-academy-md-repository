644cb89be51c16c96eb0c25c389d3e390259ec8d
ervalsa-san <ervalsananda@gmail.com>
2023-03-17 16:23:55 +0800

    Finish Data Store project

--- 2-BelajarFundamentalAplikasiAndroid/6-localdatapresistent/MyDataStore/app/src/main/java/com/palsaloid/mydatastore/MainActivity.kt
+++ 2-BelajarFundamentalAplikasiAndroid/6-localdatapresistent/MyDataStore/app/src/main/java/com/palsaloid/mydatastore/MainActivity.kt
@@ -1,11 +1,44 @@
 package com.palsaloid.mydatastore
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.CompoundButton
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
+import androidx.lifecycle.ViewModelProvider
+import com.google.android.material.switchmaterial.SwitchMaterial
 
+private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "settings")
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val switchTheme = findViewById<SwitchMaterial>(R.id.switch_theme)
+
+        val pref = SettingPreferences.getInstance(dataStore)
+        val mainViewModel = ViewModelProvider(this, ViewModelFactory(pref)).get(
+            MainViewModel::class.java
+        )
+
+        mainViewModel.getThemeSettings().observe(this,
+            { isDarkModeActive: Boolean ->
+                if (isDarkModeActive) {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                    switchTheme.isChecked = true
+                } else {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                    switchTheme.isChecked = false
+                }
+            }
+        )
+
+
+        switchTheme.setOnCheckedChangeListener { _: CompoundButton?, isChecked: Boolean ->
+            mainViewModel.saveThemeSetting(isChecked)
+        }
     }
 }
---
+++ 2-BelajarFundamentalAplikasiAndroid/6-localdatapresistent/MyDataStore/app/src/main/java/com/palsaloid/mydatastore/ViewModelFactory.kt
@@ -0,0 +1,16 @@
+package com.palsaloid.mydatastore
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+
+class ViewModelFactory(private val pref: SettingPreferences) : ViewModelProvider.NewInstanceFactory() {
+
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(MainViewModel::class.java)) {
+            return MainViewModel(pref) as T
+        }
+
+        throw IllegalArgumentException("Unknown ViewModel class: " + modelClass.name)
+    }
+}
