013ee2d1333d093b79aa887be1807e71eb7186a9
ervalsa-san <ervalsananda@gmail.com>
2023-03-22 16:22:05 +0800

    Add PendingIntent to Notification

--- 2-BelajarFundamentalAplikasiAndroid/7-backgroundtaskdanshceduler/SimpleNotif/app/src/main/java/com/palsaloid/simplenotif/MainActivity.kt
+++ 2-BelajarFundamentalAplikasiAndroid/7-backgroundtaskdanshceduler/SimpleNotif/app/src/main/java/com/palsaloid/simplenotif/MainActivity.kt
@@ -2,8 +2,11 @@ package com.palsaloid.simplenotif
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -17,8 +20,16 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun sendNotification(view: View) {
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse("http://dicoding.com"))
+        val pendingIntent = PendingIntent.getActivity(
+            this,
+            0,
+            intent,
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) PendingIntent.FLAG_IMMUTABLE else 0)
+
         val mNotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
         val mBuilder = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setContentIntent(pendingIntent)
             .setSmallIcon(R.drawable.ic_notifications)
             .setLargeIcon(BitmapFactory.decodeResource(resources, R.drawable.ic_notifications))
             .setContentTitle(resources.getString(R.string.content_title))
