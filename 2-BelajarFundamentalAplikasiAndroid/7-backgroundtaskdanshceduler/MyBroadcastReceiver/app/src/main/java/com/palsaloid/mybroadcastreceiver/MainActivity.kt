44d6273ecb143c70e3967d115f50ae39a75c008a
ervalsa-san <ervalsananda@gmail.com>
2023-03-22 15:21:15 +0800

    Finish Broadcast Receiver Project

--- 2-BelajarFundamentalAplikasiAndroid/7-backgroundtaskdanshceduler/MyBroadcastReceiver/app/src/main/java/com/palsaloid/mybroadcastreceiver/MainActivity.kt
+++ 2-BelajarFundamentalAplikasiAndroid/7-backgroundtaskdanshceduler/MyBroadcastReceiver/app/src/main/java/com/palsaloid/mybroadcastreceiver/MainActivity.kt
@@ -2,10 +2,50 @@ package com.palsaloid.mybroadcastreceiver
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import com.palsaloid.mybroadcastreceiver.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private var binding: ActivityMainBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        binding?.btnPermission?.setOnClickListener(this)
+    }
+
+    var requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted: Boolean ->
+        if (isGranted) {
+            Toast.makeText(
+                this,
+                "Sms receiver permission diterima",
+                Toast.LENGTH_SHORT
+            ).show()
+        } else {
+            Toast.makeText(
+                this,
+                "Sms receiver permission ditolak",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
+
+    override fun onClick(v: View) {
+        when (v.id) {
+            R.id.btn_permission -> requestPermissionLauncher.launch(android.Manifest.permission.RECEIVE_SMS)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
     }
 }
