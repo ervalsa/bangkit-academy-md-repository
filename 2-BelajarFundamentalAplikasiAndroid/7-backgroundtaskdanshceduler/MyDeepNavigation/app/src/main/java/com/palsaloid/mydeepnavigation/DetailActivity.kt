e9d5916197285165ae5ea809a2584f49fed5e751
ervalsa-san <ervalsananda@gmail.com>
2023-03-22 16:56:08 +0800

    Finish Deep Navigation Project

---
+++ 2-BelajarFundamentalAplikasiAndroid/7-backgroundtaskdanshceduler/MyDeepNavigation/app/src/main/java/com/palsaloid/mydeepnavigation/DetailActivity.kt
@@ -0,0 +1,28 @@
+package com.palsaloid.mydeepnavigation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.palsaloid.mydeepnavigation.databinding.ActivityDetailBinding
+
+class DetailActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDetailBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val title = intent.getStringExtra(EXTRA_TITLE)
+        val message = intent.getStringExtra(EXTRA_MESSAGE)
+
+        binding.tvTitle.text = title
+        binding.tvMessage.text = message
+    }
+
+    companion object {
+        const val EXTRA_TITLE = "extra_title"
+        const val EXTRA_MESSAGE = "extra_message"
+    }
+}
