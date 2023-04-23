ec160745be26751e2bb44816e2e5f64152eb03dd
Ervalsa Dwi Nanda <ervalsananda@gmail.com>
2023-04-23 22:18:44 +0800

    Finish UI Add Story

--- 3-BelajarPengembanganAplikasiAndroidIntermediate/submission/StoryDicoding/app/src/main/java/com/palsaloid/storydicoding/ui/add_story/AddStoryActivity.kt
+++ 3-BelajarPengembanganAplikasiAndroidIntermediate/submission/StoryDicoding/app/src/main/java/com/palsaloid/storydicoding/ui/add_story/AddStoryActivity.kt
@@ -1,25 +1,91 @@
 package com.palsaloid.storydicoding.ui.add_story
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
+import androidx.lifecycle.ViewModelProvider
+import com.palsaloid.storydicoding.HomeViewModelFactory
 import com.palsaloid.storydicoding.R
+import com.palsaloid.storydicoding.data.local.datastore.UserPreference
 import com.palsaloid.storydicoding.databinding.ActivityAddStoryBinding
+import com.palsaloid.storydicoding.utils.UserViewModel
+import java.io.File
 
+private val Context.datastore: DataStore<Preferences> by preferencesDataStore(name = "settings")
 class AddStoryActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityAddStoryBinding
+
+    private var getFile: File? = null
+    private lateinit var userViewModel: UserViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAddStoryBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        setupUserViewModel()
+
         supportActionBar?.title = "Add Story"
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        if (!allPermissionGranted()) {
+            ActivityCompat.requestPermissions(
+                this,
+                REQUIRED_PERMISSION,
+                REQUEST_CODE_PERMISSION
+            )
+        }
+    }
+
+    private fun allPermissionGranted() = REQUIRED_PERMISSION.all {
+        ContextCompat.checkSelfPermission(baseContext, it) == PackageManager.PERMISSION_GRANTED
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == REQUEST_CODE_PERMISSION) {
+            if (!allPermissionGranted()) {
+                Toast.makeText(
+                    this,
+                    "Tidak mendapatkan izin",
+                    Toast.LENGTH_SHORT
+                ).show()
+                finish()
+            }
+        }
+    }
+
+    private fun setupUserViewModel() {
+        userViewModel = ViewModelProvider(
+            this,
+            HomeViewModelFactory(
+                UserPreference.getInstance(datastore))
+        )[UserViewModel::class.java]
     }
 
     override fun onSupportNavigateUp(): Boolean {
         onBackPressed()
         return super.onSupportNavigateUp()
+    }
+
+    companion object {
+        const val CAMERA_X_RESULT = 200
+
+        private val REQUIRED_PERMISSION = arrayOf(Manifest.permission.CAMERA)
+        private const val REQUEST_CODE_PERMISSION = 10
     }
 }
