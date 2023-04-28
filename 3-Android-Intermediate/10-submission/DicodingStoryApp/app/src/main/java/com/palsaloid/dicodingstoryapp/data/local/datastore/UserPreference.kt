1a27d5b0784a63e0de698b53a4fd0a5154571881
Ervalsa Dwi Nanda <ervalsananda@gmail.com>
2023-04-28 20:08:40 +0800

    Add UserPreference

---
+++ 3-Android-Intermediate/10-submission/DicodingStoryApp/app/src/main/java/com/palsaloid/dicodingstoryapp/data/local/datastore/UserPreference.kt
@@ -0,0 +1,61 @@
+package com.palsaloid.dicodingstoryapp.data.local.datastore
+
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
+import com.palsaloid.dicodingstoryapp.data.model.UserModel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+
+class UserPreference private constructor(private val dataStore: DataStore<Preferences>){
+
+    fun getUser(): Flow<UserModel> {
+        return dataStore.data.map { preferences ->
+            UserModel(
+                preferences[NAME_KEY] ?: "",
+                preferences[TOKEN_KEY] ?: "",
+                preferences[LOGIN_STATE_KEY] ?: false
+            )
+        }
+    }
+
+    suspend fun saveUser(user: UserModel) {
+        dataStore.edit { preferences ->
+            preferences[NAME_KEY] = user.name
+            preferences[TOKEN_KEY] = user.token
+            preferences[LOGIN_STATE_KEY] = user.isLogin
+        }
+    }
+
+    suspend fun login() {
+        dataStore.edit { preferences ->
+            preferences[LOGIN_STATE_KEY] = true
+        }
+    }
+
+    suspend fun logout() {
+        dataStore.edit { preferences ->
+            preferences[LOGIN_STATE_KEY] = false
+            preferences.remove(LOGIN_STATE_KEY)
+        }
+    }
+
+    companion object {
+        @Volatile
+        private var instance: UserPreference? = null
+
+        private val NAME_KEY = stringPreferencesKey("name")
+        private val TOKEN_KEY = stringPreferencesKey("token")
+        private val LOGIN_STATE_KEY = booleanPreferencesKey("login_state")
+
+        fun getInstance(dataStore: DataStore<Preferences>) : UserPreference {
+            return instance ?: synchronized(this) {
+                val Instance = UserPreference(dataStore)
+                instance = Instance
+                Instance
+            }
+        }
+    }
+}
--- 3-Android-Intermediate/10-submission/DicodingStoryApp/app/src/main/java/com/palsaloid/dicodingstoryapp/data/remote/retrofit/ApiService.kt
+++ 3-Android-Intermediate/10-submission/DicodingStoryApp/app/src/main/java/com/palsaloid/dicodingstoryapp/data/remote/retrofit/ApiService.kt
@@ -1,4 +1,56 @@
 package com.palsaloid.dicodingstoryapp.data.remote.retrofit
 
+import com.palsaloid.dicodingstoryapp.data.remote.response.auth.LoginResponse
+import com.palsaloid.dicodingstoryapp.data.remote.response.auth.RegisterResponse
+import com.palsaloid.dicodingstoryapp.data.remote.response.story.DetailStoryResponse
+import com.palsaloid.dicodingstoryapp.data.remote.response.story.FileUploadResponse
+import com.palsaloid.dicodingstoryapp.data.remote.response.story.StoryResponse
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
+import retrofit2.Call
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Multipart
+import retrofit2.http.POST
+import retrofit2.http.Part
+import retrofit2.http.Path
+import retrofit2.http.Query
+
 interface ApiService {
+
+    @FormUrlEncoded
+    @POST("login")
+    fun login(
+        @Field("email") email: String,
+        @Field("password") password: String
+    ) : Call<LoginResponse>
+
+    @FormUrlEncoded
+    @POST("register")
+    fun register(
+        @Field("name") name: String,
+        @Field("email") email: String,
+        @Field("password") password: String
+    ) : Call<RegisterResponse>
+
+    @GET("stories?location=1")
+    fun getStoryLocation(
+        @Header("Authorization") token: String
+    ) : Call<StoryResponse>
+
+    @GET("stories/{id}")
+    fun getDetailStory(
+        @Header("Authorization") token: String,
+        @Path("id") id: String
+    ) : Call<DetailStoryResponse>
+
+    @Multipart
+    @POST("stories")
+    fun addStory(
+        @Header("Authorization") token: String,
+        @Part photo: MultipartBody.Part,
+        @Part("description") description: RequestBody
+    ) : Call<FileUploadResponse>
 }
