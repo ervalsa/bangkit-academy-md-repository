644ffb964621d5c539053f96aee48bf3e2a509b3
Ervalsa Dwi Nanda <ervalsananda@gmail.com>
2023-04-28 08:25:08 +0800

    Init Project Paging 3 Network

---
+++ 3-Android-Intermediate/10-submission/StoryDicoding/app/src/main/java/com/palsaloid/storydicoding/ui/maps/MapsActivity.kt
@@ -0,0 +1,12 @@
+package com.palsaloid.storydicoding.ui.maps
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.palsaloid.storydicoding.R
+
+class MapsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_maps)
+    }
+}
