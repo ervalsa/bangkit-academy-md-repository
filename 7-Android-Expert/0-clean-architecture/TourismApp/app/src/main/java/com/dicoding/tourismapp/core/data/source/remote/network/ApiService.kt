f4ebe01eebe7e068729cb95a5580911cd8938693
Ervalsa Dwi Nanda <ervalsananda@gmail.com>
2023-08-08 10:08:34 +0800

    feat: Change to Retrofit to get data from internet

---
+++ 7-Android-Expert/0-clean-architecture/TourismApp/app/src/main/java/com/dicoding/tourismapp/core/data/source/remote/network/ApiService.kt
@@ -0,0 +1,11 @@
+package com.dicoding.tourismapp.core.data.source.remote.network
+
+import com.dicoding.tourismapp.core.data.source.remote.response.ListTourismResponse
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface ApiService {
+
+    @GET("list")
+    fun getList(): Call<ListTourismResponse>
+}
--- 7-Android-Expert/0-clean-architecture/TourismApp/app/src/main/java/com/dicoding/tourismapp/core/di/Injection.kt
+++ 7-Android-Expert/0-clean-architecture/TourismApp/app/src/main/java/com/dicoding/tourismapp/core/di/Injection.kt
@@ -7,14 +7,18 @@ import com.dicoding.tourismapp.core.data.source.local.room.TourismDatabase
 
 import com.dicoding.tourismapp.core.data.TourismRepository
 import com.dicoding.tourismapp.core.data.source.remote.RemoteDataSource
+import com.dicoding.tourismapp.core.data.source.remote.network.ApiConfig
 import com.dicoding.tourismapp.core.utils.AppExecutors
 import com.dicoding.tourismapp.core.utils.JsonHelper
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import java.util.concurrent.TimeUnit
 
 object Injection {
     fun provideRepository(context: Context): TourismRepository {
         val database = TourismDatabase.getInstance(context)
 
-        val remoteDataSource = RemoteDataSource.getInstance(JsonHelper(context))
+        val remoteDataSource = RemoteDataSource.getInstance(ApiConfig.provideApiService())
         val localDataSource = LocalDataSource.getInstance(database.tourismDao())
         val appExecutors = AppExecutors()
 
